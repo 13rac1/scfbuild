a9e97d5c3094c1f98d9a9e6ae7b6b9b76c368541
Brad Erickson <eosrei@gmail.com>
2016-02-10 18:09:51 -0800

    build: Match/import single Unicode SVG files
    It works!
    
    All single character Unicode EmojiOne files are matched up with the correct
    GlyphID, sorted,  and added to the 'SVG ' table.
    
    * Converted to class
    * Added FontTools version check

--- svgiot-import/svgiot-import.py
+++ svgiot-import/svgiot-import.py
@@ -9,62 +9,131 @@ Copyright 2016 Brad Erickson (eosrei)
 Overall functionality:
 1. Get a list of all SVG files representing *single* unicode character emojis.
 2. Match SVG name to GlyphID using cmap table, warning on missing glyphs; SVGiOT spec requires both. 
-3. Export available character glyphs from the input_font for each SVG. 
-4. Apply transform to SVG XML to match font
-5. Add Glyph ID to SVG XML
-6. Create 'SVG ' table in font and export to output_font 
- 
+3. Apply transform to SVG XML to match font
+4. Add Glyph ID to SVG XML
+5. Create 'SVG ' table in font and export to output_font
+
+NOTE: Firefox requires restart to reload an updated font.
 """
 
-import os
-import optparse
-import glob
-
+from distutils.version import StrictVersion
+from fontTools import version
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables.S_V_G_ import table_S_V_G_
-from os.path import basename
+import glob
+from operator import itemgetter
+import optparse
+import os
+import sys
 
 
-def main(src_font, src_svg_dir, dest_font):
-    
-    svg_files = get_svg_filenames(src_svg_dir)   
-    # TODO: File exists error checking.
-    
-    font = TTFont(src_font)
-    # TODO: Drop extra tables
-        
-    #glyph_order = font.getGlyphOrder()
-    #for glyphID in range(len(glyph_order)):
-    #    print(font.getGlyphName(glyphID))
+VERSION = '0.1'
 
-    svg = '<?xml version="1.0" encoding="UTF-8"?><svg id="glyph2392" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" enable-background="new 0 0 64 64"><circle fill="#ffdd67" cx="32" cy="32" r="30"/><g fill="#664e27"><circle cx="20.5" cy="25.592" r="4.5"/><circle cx="43.5" cy="25.592" r="4.5"/><path d="m52.891 34.5c-.686 4.537-3.387 8.824-7.248 11.754-1.934 1.463-4.166 2.555-6.498 3.244-2.344.701-4.73.99-7.172 1-4.705.027-9.746-1.283-13.621-4.24-3.863-2.928-6.564-7.221-7.242-11.76 2.256 3.938 5.359 7.107 8.975 9.135 3.627 2.049 7.705 2.865 11.941 2.867 4.131.004 8.287-.814 11.891-2.863 3.616-2.028 6.72-5.201 8.974-9.139"/></g></svg>'
-    
-    table_svg = table_S_V_G_()
-    table_svg.docList = []
-    table_svg.colorPalettes = None
-    table_svg.docList.append([svg,2392,2392])
-    font['SVG '] = table_svg
-    
-    font.save(dest_font)
+# Support for SVG tables was added to fontTools in version 2.5
+min_fonttools_version = '2.5'
+if StrictVersion(min_fonttools_version) > StrictVersion(version):
+    print(
+        "ERROR: The FontTools module version must be 2.5 or higher.", file=sys.stderr)
+    sys.exit(1)
 
-def get_svg_filenames(src_svg_dir):
-    svg_files = []
-    # Match all four and five character files only
-    # TODO: Handle multi-character Unicode modifiers (colors and flags)
-    for filename in glob.glob(src_svg_dir + '/????.svg'):        
-        svg_files.append(filename)
-    for filename in glob.glob(src_svg_dir + '/?????.svg'):        
-        svg_files.append(filename)
-        
-    return svg_files
 
-if __name__ == "__main__":
-    parser = optparse.OptionParser("usage: %prog src_font src_svg_directory dest_font")
-    
+class NoCodePointsException(Exception):
+    pass
+
+
+class SVGiOTImport:
+
+    def __init__(self, src_font, src_svg_path, dest_font):
+
+        font = TTFont(src_font)
+        codepoint_names = self.get_codepoint_names(font)
+        svg_filepaths = self.get_svg_filepaths(src_svg_path)
+        svg_list = []
+
+        for file_path in svg_filepaths:
+            (filename, _) = os.path.splitext(os.path.basename(file_path))
+            # Convert unicode filename to decimal.
+            codepoint = int(filename, 16)
+            print("Current: " + filename)
+            try:
+                glyph_name = codepoint_names[codepoint]
+            except KeyError:
+                print("WARNING: No codepoint found for: {}".format(
+                    file_path), file=sys.stderr)
+                continue
+
+            glyph_id = font.getGlyphID(glyph_name)
+
+            svg = self.read_file(file_path)
+            svg = self.svg_add_glyph_id(svg, glyph_id)
+            svg = self.svg_add_xml_header(svg)
+
+            svg_list.append([svg, glyph_id, glyph_id])
+
+        svg_table = table_S_V_G_()
+        svg_table.docList = sorted(svg_list, key=lambda table: table[1])
+        svg_table.colorPalettes = None
+        font['SVG '] = svg_table
+
+        font.save(dest_font)
+
+    def get_svg_filepaths(self, src_svg_dir):
+        svg_files = []
+        # Match all four and five character files only
+        # TODO: Handle multi-character Unicode modifiers (colors and flags)
+        # using Font Ligatures
+        for filename in glob.glob(os.path.join(src_svg_dir, '????.svg')):
+            svg_files.append(filename)
+        for filename in glob.glob(os.path.join(src_svg_dir, '?????.svg')):
+            svg_files.append(filename)
+
+        return svg_files
+
+    def get_codepoint_names(self, font):
+        codepoints = {}
+        for subtable in font['cmap'].tables:
+            if subtable.isUnicode():
+                for codepoint, name in subtable.cmap.items():
+                    # NOTE: May overwrite previous values
+                    codepoints[codepoint] = name
+        if len(codepoints) is 0:
+            raise NoCodePointsException(
+                'No Unicode Code Points found in font.')
+
+        return codepoints
+
+    def read_file(self, file_path):
+        f = open(file_path, "rt")
+        data = f.read()
+        f.close()
+        return data
+
+    def svg_add_glyph_id(self, svg, id):
+        # TODO: Don't assume the glyph id is missing.
+        old = '<svg '
+        new = '<svg id="glyph{}" '.format(id)
+
+        return str.replace(svg, old, new)
+
+    def svg_add_xml_head(self, svg):
+        # TODO: Check if it's there first.
+        svg = '<?xml version="1.0" encoding="UTF-8"?>' + svg
+
+
+def main():
+    parser = optparse.OptionParser(
+        "usage: %prog src_font src_svg_directory dest_font")
+
+    # TODO: Option - Remove all glyphs not matched with SVG.
+    # TODO: Option - Apply transform all SVG files.
+
     (options, args) = parser.parse_args()
-    
+
     if len(args) != 3:
         parser.error("Incorrect number of arguments")
-    
-    main(args[0], args[1], args[2])
 
+    SVGiOTImport(args[0], args[1], args[2])
+
+
+if __name__ == "__main__":
+    main()
