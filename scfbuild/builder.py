42342ed7c35d9ec3086244a00dc4640e4beb8281
Zach DeCook <zachdecook@gmail.com>
2020-01-23 23:52:20 -0500

    chore: Make compatible with either python 2 or 3

--- scfbuild/builder.py
+++ scfbuild/builder.py
@@ -292,6 +292,8 @@ class Builder(object):
         # table__n_a_m_e.setName().
         record = NameRecord()
         # PyYAML creates strings, which are unicode as of Python3
+        if sys.version_info.major == 2:
+            text = unicode(text)
         record.string = text
         record.nameID = name_id
         record.platformID = platform_id
--- scfbuild/fforge.py
+++ scfbuild/fforge.py
@@ -10,6 +10,7 @@ from __future__ import (absolute_import, division, print_function,
 import logging
 import fontforge
 import psMat
+import sys
 
 from . import util
 from .util import FONT_EM, DEFAULT_GLYPH_WIDTH
@@ -84,7 +85,10 @@ def add_glyphs(font, svg_filepaths, conf):
             u_ids = [int(u_id, 16) for u_id in filename.split("-")]
             # Example: (0x1f441, 0x1f5e8)
 
-            u_str = ''.join(map(chr, u_ids))
+            if sys.version_info.major == 2:
+                u_str = ''.join(map(unichr, u_ids))
+            else:
+                u_str = ''.join(map(chr, u_ids))
             # Example: "U\0001f441U\0001f5e8"
 
             # Replace sequences with correct ZWJ/VS16 versions as needed
